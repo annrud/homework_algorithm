77542884540f546e29954711638cb14c199b7bbc
annrud <rudina_anna88@mail.ru>
2023-05-29 17:58:21 +0800

    Added heapsort.py

---
+++ heapsort.py
@@ -0,0 +1,33 @@
+def heapify(arr, n, i):
+    largest = i
+    left = 2 * i + 1
+    right = 2 * i + 2
+
+    if left < n and arr[i] < arr[left]:
+        largest = left
+
+    if right < n and arr[largest] < arr[right]:
+        largest = right
+
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]
+        heapify(arr, n, largest)
+
+
+def heap_sort(array):
+    n = len(array)
+
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(array, n, i)
+
+    for i in range(n - 1, 0, -1):
+        array[i], array[0] = array[0], array[i]
+        heapify(array, i, 0)
+
+    return array
+
+
+lst = [4, 1, 9, 3, 7, 5]
+print(f'Исходный список {lst}')
+heap_sort(lst)
+print(f'Отсортированный список {lst}')
